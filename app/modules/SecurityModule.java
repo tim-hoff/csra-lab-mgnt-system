b383f206ea93ef9ae4557eb6a910353bd8d49b89
Carlos <Carlos_Harris648@hotmail.com>
2016-04-09 20:15:07 -0500

    merging with CASlogin with loginhelping branch to help

--- app/modules/SecurityModule.java
+++ app/modules/SecurityModule.java
@@ -1,8 +1,11 @@
+package modules;
+
 import com.google.inject.AbstractModule;
 import org.pac4j.cas.client.CasClient;
 import org.pac4j.core.client.Clients;
 import org.pac4j.core.config.Config;
 import org.pac4j.oauth.client.CasOAuthWrapperClient;
+import controllers.HttpActionAdapter;
 import org.pac4j.play.ApplicationLogoutController;
 import org.pac4j.play.CallbackController;
 import org.pac4j.play.cas.logout.PlayCacheLogoutHandler;
@@ -13,8 +16,6 @@ import play.Environment;
 import java.io.File;
 
 public class SecurityModule extends AbstractModule {
-
-    public final static String JWT_SALT = "12345678901234567890123456789012";
 
     private final Environment environment;
     private final Configuration configuration;
@@ -31,32 +32,28 @@ public class SecurityModule extends AbstractModule {
         final String baseUrl = configuration.getString("baseUrl");
 
         // CAS
-        final CasOAuthWrapperClient casClient = new CasOAuthWrapperClient("this_is_the_key2", "this_is_the_secret2", "http://localhost:8080/cas2/oauth2.0");
-        casClient.setName("CasClient");
-        /*final CasClient casClient = new CasClient("https://casserverpac4j.herokuapp.com/login");
-        casClient.setLogoutHandler(new PlayCacheLogoutHandler());*/
-        // casClient.setGateway(true);
-        /*final CasProxyReceptor casProxyReceptor = new CasProxyReceptor();
-        casProxyReceptor.setCallbackUrl("http://localhost:9000/casProxyCallback");
-        casClient.setCasProxyReceptor(casProxyReceptor);*/
+        final CasClient casClient = new CasClient("https://cas.latech.edu:443/cas/login");
+        casClient.setLogoutHandler(new PlayCacheLogoutHandler());
 
-        final Clients clients = new Clients(baseUrl + "/callback", casClient); // , casProxyReceptor);
+        final Clients clients = new Clients("https://csra-lab-mgnt-system.herokuapp.com/callback", casClient);  //change to dashboard later
+
 
         final Config config = new Config(clients);
+        config.setHttpActionAdapter(new HttpActionAdapter());
         bind(Config.class).toInstance(config);
 
         // set profile timeout to 2h instead of the 1h default
         PlayCacheStore store = new PlayCacheStore();
         store.setProfileTimeout(7200);
         config.setSessionStore(store);
-
-        // callback
+        
+        //callback
         final CallbackController callbackController = new CallbackController();
         callbackController.setDefaultUrl("/");
         bind(CallbackController.class).toInstance(callbackController);
         // logout
         final ApplicationLogoutController logoutController = new ApplicationLogoutController();
-        logoutController.setDefaultUrl("/");
+        logoutController.setDefaultUrl("https://cas.latech.edu:443/cas/logout");
         bind(ApplicationLogoutController.class).toInstance(logoutController);
     }
 }
