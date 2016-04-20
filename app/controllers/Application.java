a7a3f238197e494059f0a169173d1ab78b9bdf46
Carlos <Carlos_Harris648@hotmail.com>
2016-04-20 15:38:15 -0500

    checking if user exists in database

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -24,6 +24,7 @@ import play.mvc.Result;
 import play.twirl.api.Content;
 import play.libs.F.Function;
 import play.libs.F.Promise;
+import play.db.jpa.Transactional;
 import play.libs.XML;
 import play.libs.XPath;
 import play.libs.ws.WSResponse;
@@ -32,6 +33,7 @@ public class Application extends UserProfileController<CommonProfile> {
 	@Inject WSClient ws;
 
     @RequiresAuthentication(clientName = "CasClient")
+    @Transactional
     public Result casIndex() {
         final CommonProfile profile = getUserProfile();
         final String service = "https://csra-lab-mgnt-system.herokuapp.com/dashboard";
