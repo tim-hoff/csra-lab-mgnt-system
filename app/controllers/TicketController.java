e3240dcf72fb9f4aedb6f770c2da449fa301668e
Tim Hoff <timothy.hoff@praeses.com>
2016-05-02 12:29:41 -0500

    Revert "TEMPORARY CAS PATCH"
    This reverts commit b2d97c68eca1523d99bbaee5c27ca2342f29a2bf.

--- app/controllers/TicketController.java
+++ app/controllers/TicketController.java
@@ -15,7 +15,7 @@ import org.pac4j.play.java.UserProfileController;
 
 public class TicketController extends UserProfileController<CommonProfile> {
 
-	// @RequiresAuthentication(clientName = "CasClient")
+	@RequiresAuthentication(clientName = "CasClient")
 	@Transactional
 	public Result index() {
 		if(!checkPrivileges())
@@ -139,12 +139,7 @@ public class TicketController extends UserProfileController<CommonProfile> {
 	//public Result getReport(Integer time) {
 		// test
 	//}
-	@Override
-	 public CommonProfile getUserProfile(){
-	 		CommonProfile com = new CommonProfile();
-	 		com.setId("box");
-	 		return com;
-	 }
+	
 	
 	//This function returns false if current user does not possess any roles
 	public boolean checkPrivileges()
